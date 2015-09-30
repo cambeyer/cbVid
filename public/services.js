a4125d9535a6de12ff2e0c5fb84bfc14f7c03e8a
Ubuntu <cam.beyer@gmail.com>
2015-09-30 20:56:41 +0000

    added progress animated icon

---
+++ public/services.js
@@ -0,0 +1,12 @@
+/*global angular*/
+angular.module('cbVidApp.services', [])
+.service('EncryptService', function ($rootScope) {
+    this.encryptedPhrases = {};
+	this.encrypt = function (text) {
+		if (!this.encryptedPhrases[text]) {
+		    /*global CryptoJS*/
+			this.encryptedPhrases[text] = CryptoJS.AES.encrypt(text, $rootScope.srpClient.getSharedKey()).toString();
+		}
+		return this.encryptedPhrases[text];
+	};
+});
