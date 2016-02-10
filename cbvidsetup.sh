024994069297e0105317afb257b9fa55e71b9373
Ubuntu <cam.beyer@gmail.com>
2016-02-10 13:31:51 +0000

    updated setup script with less sudo and more nvm

--- cbvidsetup.sh
+++ cbvidsetup.sh
@@ -7,7 +7,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confd
 sudo apt-get install build-essential libssl-dev git
 #add nvm package to install node/npm
 #change the version number according to https://github.com/creationix/nvm/releases
-sudo curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 #add nvm to the PATH
 source ~/.profile
 #install the latest stable node
@@ -30,7 +30,7 @@ sudo rm -rf ./node_modules/fluent-ffmpeg/
 git submodule add -f git://github.com/schaermu/node-fluent-ffmpeg.git node_modules/fluent-ffmpeg
 git pull && git submodule init && git submodule update && git submodule status
 cd node_modules/fluent-ffmpeg/
-sudo npm install
+npm install
 #add the cloud9 key for authorized login
 sudo echo "KEY HERE" >> ~/.ssh/authorized_keys
 #run the cloud9 installer for hooking in from the interface
@@ -38,4 +38,4 @@ sudo echo "KEY HERE" >> ~/.ssh/authorized_keys
 #install libcap2-bin to allow port 80 binding
 sudo apt-get install libcap2-bin
 #allow nodejs to bind to port 80
-sudo setcap cap_net_bind_service=+ep /usr/bin/nodejs
+sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
